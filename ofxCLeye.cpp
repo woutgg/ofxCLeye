02034d3127a7a66af9b0e1231cfdf89fe475ede2
Wouter R <gennerate@zonnet.nl>
2013-06-02 11:48:38 +0200

    Changes to get the code to compile against oF-0.7.4 (untested).

--- ofxCLeye.cpp
+++ ofxCLeye.cpp
@@ -4,260 +4,128 @@
 
 #include "ofxCLeye.h"
 
-ofxCLeye::ofxCLeye() :
-ofVideoGrabber()
-{
+ofxCLeye::ofxCLeye()
+: ofBaseVideoGrabber(), bUseThread(false), bUseTexture(true),
+  bGrabberInited(false), bChooseDevice(false), bIsFrameNew(false) {
 	attemptFramerate = 60;
 	gain = 20;
 	exposure = 511;
 }
 
-ofxCLeye::~ofxCLeye()
-{
-	if (bUseThread)
-	{
-		if (lock())
-			close();
+ofxCLeye::~ofxCLeye() {
+	if (bUseThread) {
+		if (lock()) close();
 	}
 }
 
-int ofxCLeye::listDevices()
-{
+void ofxCLeye::listDevices() {
 	int numCams = CLEyeGetCameraCount();
 
 	cout << "ofxCLeye found " << numCams << "PlayStation Eye:\n";
-	
+
 	GUID guidCam;
-	for (int i=0; i<numCams; i++)
-	{
+	for (int i=0; i<numCams; i++) {
 		guidCam = CLEyeGetCameraUUID(i);
-		printf("Camera %d GUID: [%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x]\n", 
+		printf("Camera %d GUID: [%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x]\n",
 				i, guidCam.Data1, guidCam.Data2, guidCam.Data3,
 				guidCam.Data4[0], guidCam.Data4[1], guidCam.Data4[2],
 				guidCam.Data4[3], guidCam.Data4[4], guidCam.Data4[5],
 				guidCam.Data4[6], guidCam.Data4[7]);
 	}
-
-	return numCams;
 }
 
-void ofxCLeye::grabFrame()
-{
-	if (bGrabberInited == true)
-	{
-		bIsFrameNew = false;
-
-		bool success;
-
-		if (bUseThread)
-			//presume we've already got a frame
-			success = true;
-		else
-			//get a frame
-			success = CLEyeCameraGetFrame(_cam, (PBYTE) viPixels);
-
-		
-		if (success){
-
-			bIsFrameNew = true;
-
-			if (bDoWeNeedToResize == true){
-
-				int inputW, inputH;
-				CLEyeCameraGetFrameDimensions(_cam, inputW, inputH);
-
-				float scaleW =	(float)inputW / (float)width;
-				float scaleH =	(float)inputH / (float)height;
-
-				for(int i=0;i<width;i++){
-					for(int j=0;j<height;j++){
-
-						float posx = i * scaleW;
-						float posy = j * scaleH;
-
-						/*
-
-						// start of calculating
-						// for linear interpolation
-
-						int xbase = (int)floor(posx);
-						int xhigh = (int)ceil(posx);
-						float pctx = (posx - xbase);
-
-						int ybase = (int)floor(posy);
-						int yhigh = (int)ceil(posy);
-						float pcty = (posy - ybase);
-						*/
-
-						int posPix = (((int)posy * inputW) + ((int)posx));
-
-						if (colourMode == CLEYE_MONO_PROCESSED)
-							pixels[(j*width) + i] = viPixels[posPix];
-						else {
-							pixels[(j*width)*3 + i*3 + 0] = viPixels[posPix*4 + 0];
-							pixels[(j*width)*3 + i*3 + 1] = viPixels[posPix*4 + 1];
-							pixels[(j*width)*3 + i*3 + 2] = viPixels[posPix*4 + 2];
-						}
-
-					}
-				}
-
-			} else {
-				
-				if (colourMode == CLEYE_MONO_PROCESSED)
-					memcpy(pixels, viPixels, width*height);
-				else
-				{
-					for (int i=0; i<width*height; i++)
-					{
-						pixels[i*3+0] = viPixels[i*4+2];
-						pixels[i*3+1] = viPixels[i*4+1];
-						pixels[i*3+2] = viPixels[i*4+0];
-					}
-				}
-			}
-
-			if (bUseTexture){
-				if (colourMode == CLEYE_MONO_PROCESSED)
-					tex.loadData(pixels, width, height, GL_LUMINANCE);
-				else
-					tex.loadData(pixels, width, height, GL_RGB);
-			}
-
-			if (bUseThread)
-				unlock();
-
-		}
-	}
+int ofxCLeye::getDeviceCount() {
+	return CLEyeGetCameraCount();
 }
 
-void ofxCLeye::close()
-{
-	if (bGrabberInited)
-	{
-		
-		if (bUseThread)
-			stopThread();
+void ofxCLeye::close() {
+	if (bGrabberInited) {
+		if (bUseThread) stopThread();
 
-		// Stop camera capture
-		CLEyeCameraStop(_cam);
-		// Destroy camera object
-		CLEyeDestroyCamera(_cam);
+		CLEyeCameraStop(_cam); // Stop camera capture
+		CLEyeDestroyCamera(_cam); // Destroy camera object
 		_cam = NULL;
 
 		delete[] viPixels;
+		pixels.clear();
+		tex.clear();
 	}
 
 	bGrabberInited = false;
 }
 
-bool ofxCLeye::initGrabber(int w, int h, bool useTexture, bool isGrey, bool useThread)
-{
+bool ofxCLeye::initGrabber(int w, int h, bool useTexture, bool isGrey, bool useThread) {
 	bUseTexture = useTexture;
 	bUseThread = useThread;
 
-	if (isGrey)
-		colourMode = CLEYE_MONO_PROCESSED;
-	else
-		colourMode = CLEYE_COLOR_PROCESSED;
+	if (isGrey) colourMode = CLEYE_MONO_PROCESSED;
+	else colourMode = CLEYE_COLOR_PROCESSED;
 
-	if (bChooseDevice){
-		ofLog(OF_LOG_NOTICE, "choosing %i", deviceID);
-	} else {
-		device = 0;
-	}
+	if (bChooseDevice) ofLog(OF_LOG_NOTICE, "choosing %i", deviceID);
+	else deviceID = 0;
 
 	width = w;
 	height = h;
 	bGrabberInited = false;
 
-	////////////////////////////////////////
-	// RESOLUTION
-	////////////////////////////////////////
-	if (width == 640 && height == 480)
-		_resolution=CLEYE_VGA;
-	else if (width == 320 && height == 240)
-		_resolution=CLEYE_QVGA;
-	else
-	{
+	if (width == 640 && height == 480) {
+		_resolution = CLEYE_VGA;
+	} else if (width == 320 && height == 240) {
+		_resolution = CLEYE_QVGA;
+	} else {
 		ofLog(OF_LOG_WARNING, "Selected resolution " + ofToString(width) + "x" + ofToString(height) + " is not available with ofxCLeye");
 		ofLog(OF_LOG_WARNING, "ofxCLeye using 640x480 instead");
-		_resolution=CLEYE_VGA;
+		_resolution = CLEYE_VGA;
 	}
-	////////////////////////////////////////
 
-	////////////////////////////////////////
-	// FPS
-	////////////////////////////////////////
-	if( attemptFramerate < 0)
-		return false;
-	////////////////////////////////////////
+	if( attemptFramerate < 0) return false;
 
-	////////////////////////////////////////
-	// INITIALISE
-	////////////////////////////////////////
 	GUID guid = CLEyeGetCameraUUID(deviceID);
 	_cam = CLEyeCreateCamera(guid, colourMode, _resolution, attemptFramerate);
 	bool bOk = (_cam != NULL);
-	if (bOk) bOk &= CLEyeCameraStart(_cam);
-	////////////////////////////////////////
+	if (bOk) bOk = bOk && CLEyeCameraStart(_cam);
 
-	
 	int ourRequestedWidth = width;
 	int ourRequestedHeight = height;
 
-	if (bOk == true){
-		////////////////////////////////////////
-		// REPORT GUID
-		////////////////////////////////////////
+	if (bOk == true) {
 		char guidmsg[150];
-		sprintf(guidmsg, "ofxCLeye: Camera GUID: [%08x-%04x-%04x-v%02x%02x-%02x%02x%02x%02x%02x%02x]\n", 
+		sprintf(guidmsg, "ofxCLeye: Camera GUID: [%08x-%04x-%04x-v%02x%02x-%02x%02x%02x%02x%02x%02x]\n",
 				guid.Data1, guid.Data2, guid.Data3,
 				guid.Data4[0], guid.Data4[1], guid.Data4[2],
 				guid.Data4[3], guid.Data4[4], guid.Data4[5],
 				guid.Data4[6], guid.Data4[7]);
 		ofLog(OF_LOG_VERBOSE, guidmsg);
-		////////////////////////////////////////
 
-		////////////////////////////////////////
-		// GAIN/EXPOSURE
-		////////////////////////////////////////
-		if (gain<=0) //default
-			gain=20;
-		if (exposure<=0) //default
-			exposure=511;
+		if (gain <= 0) gain = 20; //default
+		if (exposure <= 0) exposure = 511; //default
 		CLEyeSetCameraParameter(_cam, CLEYE_GAIN, gain);
 		CLEyeSetCameraParameter(_cam, CLEYE_EXPOSURE, exposure);
-		////////////////////////////////////////
 
 		CLEyeCameraGetFrameDimensions(_cam, width, height);
 		bGrabberInited = true;
 
 		//this line should also accomodate non-colour values
 		//here we init an array at the cam's native resolution
-		viPixels= new unsigned char[width * height * (isGrey ? 1 : 4)];
+		viPixels = new unsigned char[width * height * (isGrey ? 1 : 4)];
 
-		if (width == ourRequestedWidth && height == ourRequestedHeight){
-			bDoWeNeedToResize = false;
-		} else {
-			bDoWeNeedToResize = true;
-			width = ourRequestedWidth;
-			height = ourRequestedHeight;
+//		if (width == ourRequestedWidth && height == ourRequestedHeight) {
+//			bDoWeNeedToResize = false;
+//		} else {
+//			bDoWeNeedToResize = true;
+//			width = ourRequestedWidth;
+//			height = ourRequestedHeight;
+//		}
+
+		pixels.allocate(width, height, (isGrey) ? 1 : 4);
+		pixels.set(0);
+
+		if (bUseTexture) {
+			tex.allocate(width,height,(isGrey ? GL_LUMINANCE : GL_RGBA));
+			tex.loadData((unsigned char*)pixels.getPixels(), width, height, (isGrey ? GL_LUMINANCE : GL_RGBA));
 		}
 
-		pixels	= new unsigned char[width * height * (isGrey ? 1 : 3)];
-
-		if (bUseTexture){
-			// create the texture, set the pixels to black and
-			// upload them to the texture (so at least we see nothing black the callback)
-			tex.allocate(width,height,(isGrey ? GL_LUMINANCE : GL_RGB));
-
-			memset(pixels, 0, width*height * (isGrey ? 1 : 3));
-			tex.loadData(pixels, width, height, (isGrey ? GL_LUMINANCE : GL_RGB));
-		}
-
-		if (bUseThread)
-			startThread(true, false);
+		if (bUseThread) startThread(true, false);
 
 		ofLog(OF_LOG_NOTICE, "ofxCLeye created camera successfully");
 		return true;
@@ -270,30 +138,23 @@ bool ofxCLeye::initGrabber(int w, int h, bool useTexture, bool isGrey, bool useT
 }
 
 
-void ofxCLeye::videoSettings()
-{
+void ofxCLeye::videoSettings() {
 	ofLog(OF_LOG_WARNING, "ofxCLeye does not support videoSettings()");
 }
 
-void ofxCLeye::setDeviceGUID(GUID _deviceGUID)
-{
+void ofxCLeye::setDeviceGUID(GUID _deviceGUID) {
 	int numCams = CLEyeGetCameraCount();
 
-	for (int i=0; i<numCams; i++)
-		if (CLEyeGetCameraUUID(i) == _deviceGUID)
-		{
-			ofVideoGrabber::setDeviceID(i);
+	for (int i = 0; i < numCams; i++) {
+		if (CLEyeGetCameraUUID(i) == _deviceGUID) {
+			deviceID = i;
 			return;
 		}
-	
-	ofLog(OF_LOG_WARNING, "ofxCLeye cannot find camera with selected GUID");
-	deviceID		= 0;
-	bChooseDevice	= false;
-}
+	}
 
-void ofxCLeye::update()
-{
-	grabFrame();
+	ofLog(OF_LOG_WARNING, "ofxCLeye cannot find camera with selected GUID");
+	deviceID = 0;
+	bChooseDevice = false;
 }
 
 void ofxCLeye::setExposure(int _exposure)
@@ -349,12 +210,43 @@ void ofxCLeye::setAutoGain(bool _autoGain)
 		ofLog(OF_LOG_WARNING, "ofxCLeye: Attempting to set property failed, grabber not initialed");
 }
 
-void ofxCLeye::threadedFunction()
-{
-	while (isThreadRunning())
-		if (lock())
-		{
+void ofxCLeye::update() {
+	if (bGrabberInited == true) {
+		bIsFrameNew = false;
+		bool success;
+
+		if (bUseThread) success = true; //presume we've already got a frame
+		else success = CLEyeCameraGetFrame(_cam, (PBYTE) viPixels); //get a frame
+
+		if (success) {
+			bIsFrameNew = true;
+
+			pixels.setFromPixels(viPixels, width, height, (colourMode == CLEYE_MONO_PROCESSED) ? 1 : 4);
+//			if (colourMode != CLEYE_MONO_PROCESSED) {
+//				for (int i=0; i<width*height; i++) {
+//					pixels[i*3+0] = viPixels[i*4+2];
+//					pixels[i*3+1] = viPixels[i*4+1];
+//					pixels[i*3+2] = viPixels[i*4+0];
+//				}
+//			}
+
+			if (bUseTexture) {
+				tex.loadData(pixels.getPixels(), width, height,
+								(colourMode == CLEYE_MONO_PROCESSED) ? GL_LUMINANCE : GL_RGBA);
+//				if (colourMode == CLEYE_MONO_PROCESSED) tex.loadData(pixels, width, height, GL_LUMINANCE);
+//				else tex.loadData(pixels, width, height, GL_RGB);
+			}
+
+			if (bUseThread) unlock();
+		}
+	}
+}
+
+void ofxCLeye::threadedFunction() {
+	while (isThreadRunning()) {
+		if (lock()) {
 			CLEyeCameraGetFrame(_cam, (PBYTE) viPixels);
 			unlock();
 		}
+	}
 }
--- ofxCLeye.h
+++ ofxCLeye.h
@@ -8,50 +8,58 @@
 #include "CLEyeMulticam.h"
 
 #include "ofMain.h"
-
-#include "ofxThread.h"
+#include "ofThread.h"
 
 // ofxCLeye driver
 
-class ofxCLeye : public ofVideoGrabber, public ofxThread
-{
-	public:
-		ofxCLeye();
-		~ofxCLeye();
-		static int 		listDevices();
-		void			grabFrame();
-		void			close();
-		bool			initGrabber(int w, int h, bool useTexture = true, bool isGrey = false, bool useThread = true);
-		void			videoSettings();
-		void			setDeviceGUID(GUID _deviceGUID);
-		void			update();
+class ofxCLeye : public ofBaseVideoGrabber, public ofThread {
+public:
+	ofxCLeye();
+	~ofxCLeye();
+	void			listDevices();
+	int				getDeviceCount();
+	void			close();
+	bool			initGrabber(int w, int h, bool useTexture = true, bool isGrey = false, bool useThread = true);
+	void			videoSettings();
+	void			setDeviceGUID(GUID _deviceGUID);
 
-		void			setExposure(int _exposure);
-		void			setGain(int _gain);
-		void			setLED(bool _led);
-		void			setAutoExposure(bool _autoExposure);
-		void			setAutoGain(bool _autoGain);
+	void			setExposure(int _exposure);
+	void			setGain(int _gain);
+	void			setLED(bool _led);
+	void			setAutoExposure(bool _autoExposure);
+	void			setAutoGain(bool _autoGain);
 
-		int				getExposure() { return exposure; };
-		int				getGain() { return gain; };
-		bool			getLED() { return led; };
-		bool			getAutoExposure() { return autoExposure; };
-		bool			getAutoGain() { return autoGain; };
+	int				getExposure() { return exposure; };
+	int				getGain() { return gain; };
+	bool			getLED() { return led; };
+	bool			getAutoExposure() { return autoExposure; };
+	bool			getAutoGain() { return autoGain; };
 
-		void			threadedFunction();
+	void			update();
+	void			threadedFunction();
 
 
-		CLEyeCameraColorMode	colourMode;
+	CLEyeCameraColorMode	colourMode;
 
-	protected:
-		bool					bUseThread;
+protected:
+	bool					bUseThread;
+	bool					bUseTexture;
+	bool					bGrabberInited;
+	bool					bChooseDevice;
+	bool					bIsFrameNew;
 
-		CLEyeCameraInstance		_cam;
-		CLEyeCameraResolution	_resolution;
+	CLEyeCameraInstance		_cam;
+	CLEyeCameraResolution	_resolution;
 
-		bool					led;
-		int						gain, exposure;
-		bool					autoGain, autoExposure;
+	int						deviceID;
+	int						width, height;
+	int						attemptFramerate;
 
-		unsigned char			*viPixels;
+	bool					led;
+	int						gain, exposure;
+	bool					autoGain, autoExposure;
+
+	ofPixels				pixels;
+	ofTexture				tex;
+	unsigned char			*viPixels;
 };
